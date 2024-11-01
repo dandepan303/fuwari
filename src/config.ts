b14f0b51984ff74ce66b6a07e53ec06ff6e8a6f5
Dean Hiranburana <dandepan303@gmail.com>
2024-11-01 15:38:20 +0000

    Add new pfp

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Dean Hiran',
   bio: 'peripheral and pc enthusiast',
   links: [
